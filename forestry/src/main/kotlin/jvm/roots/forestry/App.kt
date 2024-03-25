249996081b34e9831aa434c0253a0e1b9ab903b9
Bradley Thompson <bradlet2@pdx.edu>
2024-03-25 07:58:43 -0700

    Add example that uses og java net http server

--- forestry/src/main/kotlin/jvm/roots/forestry/App.kt
+++ forestry/src/main/kotlin/jvm/roots/forestry/App.kt
@@ -1,6 +1,10 @@
 package jvm.roots.forestry
 
 import com.google.gson.Gson
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.net.InetSocketAddress
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -21,4 +25,14 @@ fun main(args: Array<String>) {
 
     val parsed = gson.fromJson(response.body(), Map::class.java)
     println(parsed.keys)
+
+    val httpServer = HttpServer.create(InetSocketAddress("localhost", 8080), 0)
+
+    httpServer.createContext("/") { http ->
+        val content = "Hello world! ${parsed.keys}"
+        http.sendResponseHeaders(200, content.length.toLong())
+        http.responseBody.use { it.write(content.toByteArray()) } // `use` to try-with-resources so we close out stream
+    }
+
+    httpServer.start()
 }
